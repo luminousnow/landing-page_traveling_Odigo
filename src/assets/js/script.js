1c17b8c520d235d6f0f92e3091b6707a470f0404
Denis Kravchuk <denis.kravchuk.ua@gmail.com>
2023-02-21 17:45:40 +0200

    add burger menu for the resolution less then 768px

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,11 +1,40 @@
-(function () {
-  const header = document.querySelector(".header");
+// Зміна кольору бекграунду хедера при скролі
+const header = document.querySelector(".header");
+window.onscroll = () => {
+  if (window.scrollY > 50) {
+    header.classList.add("header--scrolling");
+  } else {
+    header.classList.remove("header--scrolling");
+  }
+};
 
-  window.onscroll = () => {
-    if (window.scrollY > 50) {
-      header.classList.add("scrolling");
-    } else {
-      header.classList.remove("scrolling");
+// Мобільне меню
+let viewportWidth = window.innerWidth;
+if (viewportWidth < 768) {
+  const body = document.querySelector("body");
+  const nav = document.querySelector(".nav");
+  const mobileMenuButton = document.querySelector(".mobile-menu");
+  const openButton = document.querySelector(".mobile-menu__open-icon");
+  const closeButton = document.querySelector(".mobile-menu__close-button");
+
+  // слухаємо клік по кнопці меню
+  mobileMenuButton.addEventListener("click", onMenuClick);
+  // слухаємо натискання клавіші Esc
+  document.addEventListener("keydown", onEsc);
+
+  function onMenuClick() {
+    nav.classList.toggle("nav__open");
+    openButton.classList.toggle("mobile-menu__open-icon--active");
+    closeButton.classList.toggle("mobile-menu__close-button--active");
+    body.classList.toggle("body__lock");
+  }
+
+  function onEsc(event) {
+    if (event.key === "Escape") {
+      nav.classList.remove("nav__open");
+      openButton.classList.add("mobile-menu__open-icon--active");
+      closeButton.classList.remove("mobile-menu__close-button--active");
+      body.classList.toggle("body__lock");
     }
-  };
-})();
+  }
+}
